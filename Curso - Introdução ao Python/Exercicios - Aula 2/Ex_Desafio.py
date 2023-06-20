aa7aeb7ccdd094ab476f691d04237ad9b094db8c
Renzzo Rocha <127625871+renzzorocha@users.noreply.github.com>
2023-06-20 15:29:37 -0300

    Update Ex_Desafio.py

--- Curso - Introdução ao Python/Exercicios - Aula 2/Ex_Desafio.py
+++ Curso - Introdução ao Python/Exercicios - Aula 2/Ex_Desafio.py
@@ -1,3 +1,12 @@
+"""
+Leia atentamente as perguntas do programa para obter a melhor experiência de tal.
+Autor: Renzzo Silva Rocha
+RA: 4231925318
+Versão: 1.0
+Data de criação: 10/04/2023
+
+"""
+
 # Desafio - Gasolina
 
 # Import da biblioteca random - radint
