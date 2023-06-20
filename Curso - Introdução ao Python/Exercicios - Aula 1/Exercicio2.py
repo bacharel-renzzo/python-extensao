e0302fe370fc5556ae2f9afa1f2cfa34f01e37c3
Renzzo Rocha <127625871+renzzorocha@users.noreply.github.com>
2023-06-20 15:28:52 -0300

    Update Exercicio2.py

--- Curso - Introdução ao Python/Exercicios - Aula 1/Exercicio2.py
+++ Curso - Introdução ao Python/Exercicios - Aula 1/Exercicio2.py
@@ -1,3 +1,12 @@
+"""
+Leia atentamente as perguntas do programa para obter a melhor experiência de tal.
+Autor: Renzzo Silva Rocha
+RA: 4231925318
+Versão: 1.0
+Data de criação: 03/04/2023
+
+"""
+
 # Ex2 - Fazer um programa em que o usuário digita o raio de um circunferência e o programa imprime na tela o seu comprimento.
 
 # Importação da biblioteca math
