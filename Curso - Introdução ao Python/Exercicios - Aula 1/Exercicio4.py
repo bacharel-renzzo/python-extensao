4a494c6312b4ff2d0c42b0a7fce89771d11101d2
Renzzo Rocha <127625871+renzzorocha@users.noreply.github.com>
2023-06-20 15:29:09 -0300

    Update Exercicio4.py

--- Curso - Introdução ao Python/Exercicios - Aula 1/Exercicio4.py
+++ Curso - Introdução ao Python/Exercicios - Aula 1/Exercicio4.py
@@ -1,3 +1,12 @@
+"""
+Leia atentamente as perguntas do programa para obter a melhor experiência de tal.
+Autor: Renzzo Silva Rocha
+RA: 4231925318
+Versão: 1.0
+Data de criação: 03/04/2023
+
+"""
+
 # Ex4 - Fazer um programa em que o usuário entra com o ano de seu nascimento e o programa imprime na tela ssua idade.
 
 # Declaração da variável
