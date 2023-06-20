61d9a5d4e6efac0ee11de307b0a74a5e060fb340
Renzzo Rocha <127625871+renzzorocha@users.noreply.github.com>
2023-06-20 15:28:43 -0300

    Update Exercicio1.py

--- Curso - Introdução ao Python/Exercicios - Aula 1/Exercicio1.py
+++ Curso - Introdução ao Python/Exercicios - Aula 1/Exercicio1.py
@@ -1,3 +1,11 @@
+"""
+Leia atentamente as perguntas do programa para obter a melhor experiência de tal.
+Autor: Renzzo Silva Rocha
+RA: 4231925318
+Versão: 1.0
+Data de criação: 03/04/2023
+
+"""
 # Ex1 -  Fazer programa que converte a velocidade de m/s para km/h. Dado: velocidade(m/s) = velocidade(km/h) * 3.6
 
 # Declaração da variável velocidade
