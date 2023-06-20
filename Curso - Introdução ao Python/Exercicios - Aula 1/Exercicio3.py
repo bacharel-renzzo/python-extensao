c8586b91d1265e0f572cf0ba024ef7777066d13c
Renzzo Rocha <127625871+renzzorocha@users.noreply.github.com>
2023-06-20 15:28:58 -0300

    Update Exercicio3.py

--- Curso - Introdução ao Python/Exercicios - Aula 1/Exercicio3.py
+++ Curso - Introdução ao Python/Exercicios - Aula 1/Exercicio3.py
@@ -1,3 +1,12 @@
+"""
+Leia atentamente as perguntas do programa para obter a melhor experiência de tal.
+Autor: Renzzo Silva Rocha
+RA: 4231925318
+Versão: 1.0
+Data de criação: 03/04/2023
+
+"""
+
 # Ex3 - Fazer um programa em que o usuário entra com um número e o programa imprime na tela seu antecessor e seu sucessor.
 
 # Declaração da variável
